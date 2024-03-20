0811ffa32b691a599527e1d988210d9be696bfd0
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2024-03-20 03:16:52 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,21 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# A/B updater updatable partitions list. Keep in sync with the partition list
-# with "_a" and "_b" variants in the device. Note that the vendor can add more
-# more partitions to this list for the bootloader and radio.
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    odm \
-    product \
-    system \
-    system_ext \
-    vbmeta \
-    vbmeta_system \
-    vendor \
-    vendor_dlkm
-
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 32
 
